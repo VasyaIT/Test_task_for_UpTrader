8274235120ae6904fb947049444bea1d67212d51
Vasya <oiuy78@yandex.ru>
2023-04-27 00:54:19 +0300

    Let's gooo!!!

--- tree_menu/tree_menu/urls.py
+++ tree_menu/tree_menu/urls.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
-from menu import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', include('menu.urls')),
     path('admin/', admin.site.urls),
 ]
